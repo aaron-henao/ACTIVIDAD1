1cd170f41c2c4d1cc09e544bfbe1feb126a0e869
AARON HENAO <aaronhenao27@gmail.com>
2023-02-08 10:39:59 -0500

    Commit ejercicio 5

---
+++ ejercicios/ejercicio_5.py
@@ -0,0 +1,8 @@
+def degress():
+    celsius = (fahrenheit - 32) * 5/9
+    print("Degress in celsius ", celsius)
+
+fahrenheit= float(input(("Enter degress in Fahrenheit to convert: ")))
+degress()
+
+
