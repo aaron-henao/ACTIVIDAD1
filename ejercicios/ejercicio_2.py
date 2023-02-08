f73d7db8b2f8beff2fdf62efca87c0b5d6b69a41
AARON HENAO <aaronhenao27@gmail.com>
2023-02-08 10:23:48 -0500

    Commit ejercicio 2

---
+++ ejercicios/ejercicio_2.py
@@ -0,0 +1,15 @@
+import math
+
+
+def radius():
+    radio = float(input("Enter radius value: "))
+    pi = math.pi
+    formula = math.pi * radio ** 2
+
+    print (f"Circumference area with radius {radio} is {formula}")
+
+print ("This program is to find de area of a circle")
+radius()
+
+
+
