f610477917f57268d1291d509883318e3cefca25
AARON HENAO <aaronhenao27@gmail.com>
2023-02-08 20:37:22 -0500

    Commit ejercicio 13

---
+++ ejercicios/ejercicio_13.py
@@ -0,0 +1,10 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+sum = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print(f"sum = {sum}  substraction={subtraction}  multiplication={multiplication}  division={division}")
+
